86e030b8a0c002fbbe4218d3b8502626e8241972
Elias Vasquez <evasq710@hotmail.com>
2023-04-14 01:15:20 -0600

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import './index.css'
 // import pages
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename='/Banco-Errores'>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
