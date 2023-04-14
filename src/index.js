7f318752116e902523577009213caa78ab63baea
Elias Vasquez <evasq710@hotmail.com>
2023-04-14 01:08:55 -0600

    Basename Router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/Banco-Errores'>
       <App />
     </Router>
   </React.StrictMode>
