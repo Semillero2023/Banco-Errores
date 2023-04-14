e9223ca2d1efa8812206015ceddef3e70842c08e
Elias Vasquez <evasq710@hotmail.com>
2023-04-14 00:24:17 -0600

    Hash Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css'
 // import pages
 import Home from "./pages/Home";
