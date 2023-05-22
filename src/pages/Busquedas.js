70e28154eef7c82d882cec15f95d5a935486a038
Santiago78op <2993696170101@ingenieria.usac.edu.gt>
2023-05-21 18:43:21 -0600

    pureba

--- src/pages/Busquedas.js
+++ src/pages/Busquedas.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 class Busquedas extends React.Component {
-    render(){
+    render() {
 
-        return(
+        return (
+            // arrayErrores.forEach((doc) => {
             <></>
         );
     }
