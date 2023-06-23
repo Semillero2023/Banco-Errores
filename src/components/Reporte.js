9792282bdc4e6384eede53846b30b4dbd2583257
Elias Vasquez <evasq710@hotmail.com>
2023-06-22 21:00:59 -0600

    Cambio 'src' Font Roboto

--- src/components/Reporte.js
+++ src/components/Reporte.js
@@ -10,7 +10,7 @@ class Reporte extends React.Component {
         //Plantilla de estilos
         Font.register({
           family: 'Roboto',
-          src: 'https://fonts.gstatic.com/s/roboto/v16/zN7GBFwfMP4uA6AR0HCoLQ.ttf'
+          src: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap'
         });
         
         const styles = StyleSheet.create({
